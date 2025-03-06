d6ba8d164f25449eea8633a054ffc327c5092733
nam <trinhvietnambk@gmail.com>
2025-03-07 06:36:21 +0700

    +

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,10 @@
 declare module "react-native-thermal-usb" {
   export class USBPrinter {
-    static init():
-      | void
-      | PromiseLike<void>
-      | PromiseLike<string>;
-    static getDeviceList(): void | string | PromiseLike<void> | PromiseLike<any>;
+    static init(): PromiseLike<void>;
+    static getDeviceList(): PromiseLike<any>;
     static connectPrinter(
       vendorId: string, productId: string
     ): PromiseLike<any>;
-    static printerLineSpace(
-      space: number
-    ): void | string | PromiseLike<void> | PromiseLike<string>;
     static closeConn(): PromiseLike<void>;
     static printRawData(
       data: string
